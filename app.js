df3d930e77743633c3b892cc35b07fbc855980ac
ruhinalio <116165822+ruhinalio@users.noreply.github.com>
2023-02-24 15:06:23 +0400

    last comit

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+// choose elements
 let nameSide = document.querySelector(".name");
 let pictureSide = document.querySelector(".picture");
 let idSide = document.querySelector(".id");
 let bioSide = document.querySelector(".bio");
 let companySide = document.querySelector(".company");
 let blogSide = document.querySelector(".blog");
-
+// comming data from api.github
 fetch("https://api.github.com/users/amilabdullazadeh")
   .then((res) => res.json())
   .then((data) => {
     nameSide.innerText = `${data.name}`;
-    pictureSide.innerText = `${data.avatar_url}`;
     idSide.innerText = `${data.id}`;
     bioSide.innerText = `${data.bio}`;
     companySide.innerText = `${data.company}`;
-    blogSide.innerText = `${data.blog}`;
+
+    // create new img and this img adding pictureSide
+    let sekil = document.createElement("img");
+    sekil.src = `${data.avatar_url}`;
+    pictureSide.appendChild(sekil);
+    
+    //  crate new a'tag' and adding blogSide
+    let linkedin = document.createElement("a");
+    linkedin.href = `${data.blog}`;
+    linkedin.innerText = " page";
+    blogSide.appendChild(linkedin);
   })
   .catch((error) => console.error(error));
